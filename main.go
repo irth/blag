24e2fccdda0fb984a9490731388e54099d0718ee
Wojciech Kwolek <me@irth.pl>
2015-08-02 16:23:11 +0200

    Gofmt; Added LoadTheme.

--- main.go
+++ main.go
@@ -1,28 +1,44 @@
 package main
 
 import (
-    "flag"
+	"flag"
+	"github.com/flosch/pongo2"
+	"path"
 )
 
 type Config struct {
-    Input *string
-    Theme *string
-    Output *string
+	Input  *string
+	Theme  *string
+	Output *string
 }
 
 type BlagPost struct {
-    Title string
-    Timestamp int
-    Author string
-    Content string
-    Slug string
+	Title     string
+	Timestamp int
+	Author    string
+	Content   string
+	Slug      string
 }
 
+type Theme struct {
+	Page *pongo2.Template
+	Post *pongo2.Template
+}
 
-var config Config
+func LoadTheme(theme_dir string) Theme {
+	t := Theme{}
+	t.Page = pongo2.Must(pongo2.FromFile(path.Join(theme_dir, "page.html")))
+	t.Post = pongo2.Must(pongo2.FromFile(path.Join(theme_dir, "post.html")))
+	return t
+}
+
 func main() {
-    config.Input = flag.String("input", "./input/", "Directory where blog posts are stored (in markdown format)");
-    config.Output = flag.String("output", "./output/", "Directory where generated html should be stored");
-    config.Theme = flag.String("theme", "./theme/", "Directory containing theme files (templates)");
-    flag.Parse();
+	var config Config
+	config.Input = flag.String("input", "./input/", "Directory where blog posts are stored (in markdown format)")
+	config.Output = flag.String("output", "./output/", "Directory where generated html should be stored")
+	config.Theme = flag.String("theme", "./theme/", "Directory containing theme files (templates)")
+	flag.Parse()
+
+	var theme Theme
+	theme = LoadTheme(*config.Theme)
 }
